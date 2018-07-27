746bde3ef5f90bc1ba84542c9573373cf6eba757
roshni0107 <41774774+roshni0107@users.noreply.github.com>
2018-07-27 12:23:21 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SearchForCountry.Models;
+
+namespace SearchForCountry.Controllers
+{
+    public class HomeController : Controller
+    {
+        TestEntities db = new TestEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult login()
+        {
+            ViewBag.Message = "Your login page.";
+
+            return View();
+        }
+        [HttpPost]
+        public JsonResult SaveData(Account model)
+        { 
+            db.Accounts.Add(model);
+            db.SaveChanges();
+            return Json("Registration Successfull",JsonRequestBehavior.AllowGet);
+        }
+    }
+   
+}
